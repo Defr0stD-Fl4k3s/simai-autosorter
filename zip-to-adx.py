c73b59b4d7ac41d55f59bb0b63aded71616773cd
Jon Demetrei Bonjoc <bonjoc.jondem@gmail.com>
2025-03-07 16:27:09 +0800

    Directory updates, ZIP Import checker and .gitignore update

--- zip-to-adx.py
+++ zip-to-adx.py
@@ -1,5 +1,14 @@
 import os
+import warnings
 from pathlib import Path
 
+# Make directories if not existing
 os.makedirs("ZIP Imports", exist_ok=True)
 os.makedirs("ADX Files", exist_ok=True)
+
+zip_import_dir = "./ZIP Imports"
+adx_output_dir = "./ADX Files"
+
+if not os.listdir(zip_import_dir):
+    warnings.warn("No imported ZIPs found!")
+    exit(1001)
