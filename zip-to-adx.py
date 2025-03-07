fa9d277b5c109bb03827ccc9b0e9f9470403626e
Jon Demetrei Bonjoc <bonjoc.jondem@gmail.com>
2025-03-07 17:15:39 +0800

    Added comments & Added file unzipper

--- zip-to-adx.py
+++ zip-to-adx.py
@@ -1,14 +1,24 @@
 import os
 import warnings
+import zipfile
 from pathlib import Path
 
 # Make directories if not existing
 os.makedirs("ZIP Imports", exist_ok=True)
 os.makedirs("ADX Files", exist_ok=True)
 
+# Directory variables
 zip_import_dir = "./ZIP Imports"
 adx_output_dir = "./ADX Files"
 
+# Check if 'ZIP Imports' folder is empty
 if not os.listdir(zip_import_dir):
     warnings.warn("No imported ZIPs found!")
     exit(1001)
+
+# Unzip all .zip files found in 'ZIP Imports' folder
+for zip_file in os.listdir(zip_import_dir):
+    # print(zip_file)
+    zip_file_dir = os.path.join(zip_import_dir, zip_file)
+    with zipfile.ZipFile(zip_file_dir, "r") as extract_zip:
+        extract_zip.extractall(path=adx_output_dir)
